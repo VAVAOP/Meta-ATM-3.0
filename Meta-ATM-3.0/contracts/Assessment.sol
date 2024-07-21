be6dfd082394838d377bb772f21753dabab5af35
VAVAOP <119060354+VAVAOP@users.noreply.github.com>
2024-07-21 13:50:33 +0530

    Assessment.sol

--- Meta-ATM-3.0/contracts/Assessment.sol
+++ Meta-ATM-3.0/contracts/Assessment.sol
@@ -10,7 +10,8 @@ contract Assessment {
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-  
+    
+
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
@@ -20,14 +21,8 @@ contract Assessment {
     function blocktime() public view returns(uint){
         return block.timestamp;
     }
-    function messagevalue() payable  external returns(uint){
-        return msg.value;
-    }
-    function txgasprice() public view returns(uint){
-        return tx.gasprice;
-    }
-    function txorigin() public view returns(address){
-        return tx.origin;
+    function sendowner() public view returns(address){
+        return owner;
     }
     function chainid() public view returns(uint){
         return block.chainid;
@@ -41,14 +36,20 @@ contract Assessment {
     function gas_left() public view returns (uint256){
         return gasleft();
     }
-
-    function transfer(address payable recipient ,uint amount) public payable{
-       
-       amount=msg.value;
-
-        recipient.transfer(msg.value);
-
-        
+    function simpleInterest(uint p,uint t) pure  public  returns(uint){
+        uint I;
+        I = (p*80*t)/100;
+        return I;
+    }
+    function compoundInterest(uint p,uint t)pure public returns(uint){
+        uint C;
+        uint s;
+        uint q;
+        uint r = 80;
+        s=r/100; // 80%~100%//
+        q=2;
+        C=p*(q)**t;
+        return C;
     }
 
     function getBalance() public view returns(uint256){
